6a4ba341d1aac5a44a828ad16ef98690c9b4d0bd
akramkhalifa <akramkhalifa3@gmail.com>
2016-03-24 19:14:02 +0000

    completed parse son

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Akram khalifa on 24/03/2016.
+//  Copyright © 2016 Akram khalifa. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
+
